2338a02271606995980fffd59b326e5ec057416b
sanniabdullateef <slateef39@gmail.com>
2020-04-18 17:17:56 +0100

    second commit

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,59 @@
 const express = require ('express');
-const { MongoClient } = require(mongodb);
+const { MongoClient } = require('mongodb');
 const debug = require('debug')('app:authRoutes');
+const passport = require('passport')
 
-const authRoutes = express.Router();
+const authRouter = express.Router();
 
-function router () {
-    authRouter.route('/singUp')
-    .post
-};
+function router (nav) {
+    authRouter.route('/signUp')
+    .post((req, res) => {
+        const { username, password } = req.body;
+        const url = 'mongodb://localhost:27017';
+        const dbName = 'libraryApp';
 
-module.exports router;
+        (async function addUser(){
+            let client;
+            try{
+                client = await MongoClient.connect(url);
+                debug('Connected correctly to server');
+
+                const db = client.db(dbName);
+
+                const col = db.collection('users');
+                const user = { username , password };
+                const results = await col.insertOne(user);
+                req.login(results.ops[0], ()=>{
+                    res.redirect('/auth/profile')
+                    debug(results)
+                });
+            } catch(err) {
+                debug(err);
+            }
+        }()); 
+    });
+    authRouter.route('/signin')
+    .get((req, res) => {
+        res.render('signin', {
+            nav,
+            title: 'Sign In'
+        });
+    })
+    .post(passport.authenticate('local', {
+        successRedirect: '/auth/profile',
+        failureRedirect: '/'
+    }));
+    authRouter.route('/profile')
+    .all((req, res, next) =>{
+        if(req.user){
+            next();
+        }
+        res.redirect('/')
+    })
+    .get((req, res) =>{
+        res.json(req.user); 
+    });
+    return authRouter;
+}
+
+module.exports = router;
